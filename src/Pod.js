dac18f45f09f484c49df5f4288c037ece43cdaed
GabGo51 <snow.gabo51@gmail.com>
2023-05-23 16:50:43 -0400

    made it move

--- src/Pod.js
+++ src/Pod.js
@@ -1,24 +1,67 @@
-import { styled } from "styled-components";
+import  styled  from "styled-components";
 import { useEffect, useState, useRef } from "react";
 
 const Pod = () => {
   const podRef = useRef(null);
   const [position, setPosition] = useState({ x: 0, y: 0 });
+  const [isMouseOnScreen, setIsMouseOnScreen] = useState(true);
 
   useEffect(() => {
-    const handleMouse = (event) => {
-      const { clientX, clientY } = event;
+    const handleMouseEnter = () => {
+      setIsMouseOnScreen(true);
     };
 
+    const handleMouseLeave = () => {
+      setIsMouseOnScreen(false);
+    };
+
+    const handleMouse = (event) => {
+      const { clientX, clientY } = event;
+      setPosition({ x: clientX, y: clientY })
+      setIsMouseOnScreen(true);
+    };
+
+    
+
+
+    document.addEventListener("mouseenter", handleMouseEnter);
+    document.addEventListener("mouseleave", handleMouseLeave);
     document.addEventListener('mousemove',handleMouse)
+    
 
     return () =>{
+      document.removeEventListener("mouseenter", handleMouseEnter);
+      document.removeEventListener("mouseleave", handleMouseLeave)
       document.removeEventListener('mousemove',handleMouse)
     }
   }, []);
 
+  useEffect(() => {
+    const { current: podElement } = podRef;
 
-  return <Box>AYO</Box>;
+    const movePod = () => {
+      const { x, y } = position;
+      const podWidth = podElement.offsetWidth;
+      const podHeight = podElement.offsetHeight;
+      const offsetX = (x- window.innerWidth / 2 ) / 10;
+      const offsetY = (y-window.innerHeight / 2) / 10;
+
+      if (isMouseOnScreen) {
+        podElement.style.transform = `translate(${offsetX}px, ${offsetY}px)`;
+      } else {
+        podElement.style.transform = "none";
+      }
+    };
+
+    movePod();
+
+    return () => {
+      podElement.style.transform = 'none';
+    };
+  }, [position]);
+
+
+  return <Box ref={podRef}>AYO</Box>;
 };
 
 const Box = styled.div`
