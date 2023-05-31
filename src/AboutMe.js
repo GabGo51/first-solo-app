b4ea20ff1ed0c4e43ba677ee91428798d6a7b09c
GabGo51 <snow.gabo51@gmail.com>
2023-05-31 11:52:30 -0400

    About me Done

--- src/AboutMe.js
+++ src/AboutMe.js
@@ -1,47 +1,83 @@
-import  styled  from "styled-components"
-import boutme from "./Images/boutme.jpg"
+import styled from "styled-components";
+import boutme from "./Images/boutme.jpg";
+import Icon from "react-icons-kit";
+import { spinner8 } from "react-icons-kit/icomoon/spinner8";
+import { keyframes } from "styled-components";
 const AboutMe = () => {
-
-return(
-
-  <Container>
-    <Image src={boutme} alt="Me boarding"/>
-    <TextBox>
-      <Title>AYOO</Title>
-      <Text></Text>
-    </TextBox>
-    
-  </Container>
-
-
-)
-}
+  return (
+    <Container>
+      <Image src={boutme} alt="Me boarding" />
+      <Circle icon={spinner8} size={200}  />
+      <TextBox>
+        <Title>ABOUT ME</Title>
+        <Text>
+          The sun was shining brightly as I walked through the bustling city
+          streets. People hurried past me, their footsteps echoing against the
+          pavement. I marveled at the diversity of the crowd, each person with
+          their own story and destination. I decided to take a detour and
+          explore a nearby park. The air was filled with the sweet scent of
+          blooming flowers, and the sound of birds chirping provided a soothing
+          melody. I found a peaceful spot under a tall oak tree and sat down,
+          closing my eyes to soak in the tranquility.
+        </Text>
+      </TextBox>
+    </Container>
+  );
+};
 
 const Container = styled.div`
-margin-top: 800px;
-width: 1000px;
-padding-bottom: 1000px;
-display: flex;
-`
+  margin-top: 800px;
+  width: 600px;
+  padding-bottom: 200px;
+  display: flex;
+  position: relative;
+`;
 
 const Image = styled.img`
-width: 450px;
-height: 600px;
-border-radius: 20px;
-object-fit: cover;
-margin-left: -30px;
-`
+  width: 450px;
+  height: 600px;
+  border-radius: 20px;
+  object-fit: cover;
+  margin-left: -250px;
+  margin-right: 100px;
+`;
 
-const TextBox = styled.div`
+const rotate360 = keyframes`
+  0% {
+    transform: rotate(0deg);
+  }
+  25% {
+    transform: rotate(100deg);
+  }
+  50% {
+    transform: rotate(180deg);
+  }
+  75% {
+    transform: rotate(230deg);
+  }
+  100% {
+    transform: rotate(360deg);
+  }
+`;
 
-`
+const Circle = styled(Icon)`
+  position: absolute;
+  bottom: 90px;
+  margin-left: 95px;
+  animation: ${rotate360} 2s linear infinite;
+  color: white;
+  opacity: 0.8;
+`;
+
+const TextBox = styled.div``;
 
 const Title = styled.h2`
-
-`
+  text-decoration: underline;
+  margin-bottom: 40px;
+`;
 
 const Text = styled.p`
+  font-size: 1.3em;
+`;
 
-`
-
-export default AboutMe
+export default AboutMe;
